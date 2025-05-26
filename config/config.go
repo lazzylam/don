3a57f51dc0efa9b82ed94e714dd116acd6700afe
Lamora <itsmerick184@gmail.com>
2025-05-26 15:33:50 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -10,8 +10,8 @@ type Config struct {
 
 func LoadConfig() *Config {
     return &Config{
-        BotToken:      "YOUR_BOT_TOKEN",
-        MongoDBURI:    "mongodb+srv://myuser:mypassword@cluster0.mongodb.net/?retryWrites=true&w=majority",
+        BotToken:      "7930223337:AAFkF8FO4ijKNYHd4zApmY4XPEodXSJbZAI",
+        MongoDBURI:    "mongodb+srv://itsmerick184:dummylove@cluster0.b3tg7rz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         DBName:        "anti_gcast_bot",
         MaxGoroutines: 100,
     }
