33f16c0506c60b7212e1e6fb3e3592f5dc1a52de
Lamora <itsmerick184@gmail.com>
2025-05-26 17:29:56 +0700

    Update regex.go

--- utils/regex.go
+++ utils/regex.go
@@ -4,14 +4,7 @@ import "regexp"
 
 var (
     LinkRegex    = regexp.MustCompile(`(?i)(https?://|www\.)[^\s]+`)
-    MentionRegex = regexp.MustCompile(`(?i)@[a-zA-Z0-9_]+`)
+    MentionRegex = regexp.MustCompile(`@[a-zA-Z0-9_]+`)
     TMORegEx     = regexp.MustCompile(`(?i)\b(tmo|telegram messenger offline)\b`)
-    JoinRegex    = regexp.MustCompile(`(?i)\b(join|gabung|add)\b`)
+    JoinRegex    = regexp.MustCompile(`(?i)\b(join|gabung|add|tmo|vcs|sange|anjing)\b`)
 )
-
-func IsGCASTMessage(text string) bool {
-    return LinkRegex.MatchString(text) || 
-           MentionRegex.MatchString(text) || 
-           TMORegEx.MatchString(text) || 
-           JoinRegex.MatchString(text)
-}
